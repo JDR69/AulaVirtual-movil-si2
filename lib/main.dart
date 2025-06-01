238bc3448f7a7a7a02707c276601212b4d1e175e
Joan Dr <joandanielrr@gmail.com>
2025-06-01 15:54:08 -0400

    Firebase 100

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,14 @@ import 'screens/Activities.dart';
 import 'screens/Notebooks.dart';
 import 'screens/user_profile_screen.dart';
 import 'provider/user_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MultiProvider(
       providers: [ChangeNotifierProvider(create: (_) => UserProvider())],
@@ -35,7 +41,6 @@ class MyApp extends StatelessWidget {
         '/activities': (context) => ActivitiesScreen(),
         '/grades': (context) => GradesScreen(),
         '/profile': (context) => UserProfileScreen(),
-      
       },
     );
   }
